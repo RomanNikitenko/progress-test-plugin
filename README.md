553a82561f9f08207f04e8139431d09235e1901e
Roman Nikitenko <rnikiten@redhat.com>
2020-11-02 22:00:30 +0200

    Add plugin for testing 'withProgress' functionality
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+# Theia - progress-test-plugin
+Progress-test-plugin example for Theia.
+
+The plugin allows to test plugin API changes related to [withProgress](https://github.com/eclipse-theia/theia/blob/bdc9aa276cb55e5cf3dc8d7cb6e795ed8ffba998/packages/plugin/src/theia.d.ts#L3954) function.
+
+At the moment it's possible to use [ProgressLocation](https://github.com/eclipse-theia/theia/blob/bdc9aa276cb55e5cf3dc8d7cb6e795ed8ffba998/packages/plugin/src/theia.d.ts#L4172-L4185) enum only as a location at which progress should show.
+
+[The PR](https://github.com/eclipse-theia/theia/pull/8700) is aligning [location field](https://github.com/eclipse-theia/theia/blob/bdc9aa276cb55e5cf3dc8d7cb6e795ed8ffba998/packages/plugin/src/theia.d.ts#L4154) and the corresponding behavior with VS Code.
+
+Current plugin contains a few commands to have an ability to test [The PR](https://github.com/eclipse-theia/theia/pull/8700).
+
+You can use:
+- Start progress for Explorer view
+- Start progress for Search view
+- Start progress for Debug view
+- Start progress for SCM view
+
+commands from Command Palette (F1) to check progress in the correcponding view. 
+
+First 3 commands use `viewId` (new behavior) to show progress while running the given callback, the last command uses [ProgressLocation](https://github.com/eclipse-theia/theia/blob/bdc9aa276cb55e5cf3dc8d7cb6e795ed8ffba998/packages/plugin/src/theia.d.ts#L4172-L4185) enum to show progress for SCM view (existed behavior). 
+
---
+++ src/progress-test-plugin-backend.ts
@@ -0,0 +1,69 @@
+
+/**
+ * Generated using theia-plugin-generator
+ */
+
+import * as theia from '@theia/plugin';
+
+export function start(context: theia.PluginContext) {
+    const DELAY = 5000;
+
+    const explorerStartProgressCommand = {
+        id: 'start-progress-explorer',
+        label: "Start progress for Explorer view"
+    };
+
+    const searchStartProgressCommand = {
+        id: 'start-progress-search',
+        label: "Start progress for Search view"
+    };
+
+    const debugStartProgressCommand = {
+        id: 'start-progress-debug',
+        label: "Start progress for Debug view"
+    };
+
+    const scmStartProgressCommand = {
+        id: 'start-progress-scm',
+        label: "Start progress for SCM view"
+    };
+
+    context.subscriptions.push(theia.commands.registerCommand(explorerStartProgressCommand, () => {
+        theia.commands.executeCommand('fileNavigator:toggle');
+        theia.window.withProgress({ location: { viewId: 'explorer' } }, async () => {
+
+            return timeout(DELAY);
+        });
+    }));
+
+    context.subscriptions.push(theia.commands.registerCommand(debugStartProgressCommand, () => {
+        theia.commands.executeCommand('debug:toggle');
+        theia.window.withProgress({ location: { viewId: 'debug' } }, async () => {
+            return timeout(DELAY);
+        });
+    }));
+
+    context.subscriptions.push(theia.commands.registerCommand(searchStartProgressCommand, () => {
+        theia.commands.executeCommand('search-in-workspace.toggle');
+        theia.window.withProgress({ location: { viewId: 'search', } }, async () => {
+            return timeout(DELAY);
+        });
+    }));
+
+    context.subscriptions.push(theia.commands.registerCommand(scmStartProgressCommand, () => {
+        theia.commands.executeCommand('scmView:toggle');
+        theia.window.withProgress({ location: theia.ProgressLocation.SourceControl }, async () => {
+            return timeout(DELAY);
+        });
+    }));
+}
+
+export function timeout(ms: number): Promise<void> {
+    return new Promise<void>((resolve) => {
+        setTimeout(() => resolve(), ms);
+    });
+}
+
+export function stop() {
+
+}
